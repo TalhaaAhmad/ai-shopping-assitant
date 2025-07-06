437eed4293e75b3acebbc7cf02f608268a31eba9
Talha Ahmad <121342513+TalhaaAhmad@users.noreply.github.com>
2025-07-06 14:58:49 +0500

    Delete test_tools directory

--- test_tools/products.ts
+++
@@ -1,8 +0,0 @@
-   // convex/products.ts
-   import { query } from "./_generated/server";
-   export const getAllProducts = query({
-     args: {},
-     handler: async (ctx) => {
-       return await ctx.db.query("products").collect();
-     },
-   });
